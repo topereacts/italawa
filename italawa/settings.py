5e069bbd24440f3d83e67b07cdef42ffb92a4e3d
topereacts <108821725+topereacts@users.noreply.github.com>
2024-12-21 01:55:14 -0800

    Update settings.py

--- italawa/settings.py
+++ italawa/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jessiecolter8@gmail.com'
-EMAIL_HOST_PASSWORD = 'ecdmzpulixdnohvp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 AUTH_USER_MODEL = 'events.User'
